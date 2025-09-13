56db29c9d91010906717f598f694c2abe04442da
baikoby228 <baikoby228@gmail.com>
2025-09-13 17:07:47 +0300

    Refactor __init__.py

--- app/processing/command_processing/command_menu/__init__.py
+++ app/processing/command_processing/command_menu/__init__.py
@@ -3,5 +3,7 @@ from .callback_menu_info import processing_callback_menu_info
 from .callback_menu_set import processing_callback_menu_set
 from .callback_menu_del import processing_callback_menu_del
 
-__all__ = ['processing_command_menu', 'processing_callback_menu_info', 'processing_callback_menu_set',
-           'processing_callback_menu_del']
+__all__ = [
+    'processing_command_menu', 'processing_callback_menu_info', 'processing_callback_menu_set',
+    'processing_callback_menu_del'
+]
--- config/__init__.py
+++ config/__init__.py
@@ -1,2 +1,3 @@
 from .global_constants import MAX_AMOUNT_OF_PRODUCTS
+
 __all__ = ['MAX_AMOUNT_OF_PRODUCTS']
