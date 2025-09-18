dc328897adb303215290a83d9a41ba8ae6e073c7
baikoby228 <baikoby228@gmail.com>
2025-09-19 00:40:28 +0300

    Update message_text in /start, /help

--- app/processing/command_start.py
+++ app/processing/command_start.py
@@ -13,10 +13,10 @@ bot = Bot(token=API_TOKEN)
 
 async def processing_command_start(user_id: int, chat_id: int) -> None:
     text = (
-        'Привет я бот sale hunter, я помогу тебе выгодно закупаться на маркетплейсах\n'
+        'Привет! Я бот Sale Hunter, я помогу тебе выгодно закупаться на маркетплейсах\n'
         '<code>/help</code> для подробной инструкции пользования ботом\n'
-        '<code>/settings</code> для настраивания бота\n'
         '<code>/menu</code> для открытия меню отслеживаемых товаров\n'
+        '<code>/settings</code> для настраивания бота\n'
         '<code>/add</code> для добавления товара в список отслеживаемых\n'
         '<code>/remove</code> для удаления товара из списока отслеживаемых\n'
         '<code>/change</code> для изменения отслеживаемой цены у товара'
